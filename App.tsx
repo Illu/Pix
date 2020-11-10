f0c6ba7a6f9f04c239f4190dd609d3d677b95e8d
Maxime Nory <mail@maximenory.com>
2020-11-10 22:38:43 +0100

    chore: add navigation

--- App.tsx
+++ App.tsx
@@ -3,9 +3,26 @@ import {ThemeProvider} from 'styled-components/native';
 import {Themes} from './src/types';
 import {StatusBar} from 'react-native';
 import {darkTheme, lightTheme} from './src/theme';
+import {NavigationContainer} from '@react-navigation/native';
 import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
 import AppState from './src/stores/AppState';
 import Home from './src/screens/Home';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
+import {enableScreens} from 'react-native-screens';
+
+enableScreens();
+
+const Tab = createBottomTabNavigator();
+const HomeNav = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <HomeNav.Navigator>
+      <HomeNav.Screen name="Home" component={Home} />
+    </HomeNav.Navigator>
+  );
+};
 
 const App = () => {
   const scheme = useColorScheme();
@@ -32,8 +49,13 @@ const App = () => {
   return (
     <AppearanceProvider>
       <ThemeProvider theme={theme}>
-        <StatusBar barStyle={statusBarStyle} />
-        <Home />
+        <NavigationContainer>
+          <StatusBar barStyle={statusBarStyle} />
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="Challenges" component={HomeStack} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
     </AppearanceProvider>
   );
