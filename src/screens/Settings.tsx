104c44ad936bd76e6d199038319848dbc340902f
Maxime Nory <mail@maximenory.com>
2020-12-10 21:37:08 +0100

    feat: dynamic tips page content

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,7 +1,7 @@
 import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import { observer } from 'mobx-react';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Alert, Linking } from 'react-native';
 import * as RNIap from 'react-native-iap';
 import Rate from 'react-native-rate';
@@ -20,18 +20,18 @@ const logout = () => {
 const Settings = observer(() => {
   const navigation = useNavigation();
   const userStore = useContext(User);
+  const [products, setProducts] = useState<RNIap.Product<string>[]>();
+
   const items = [
     [
       {
         title: 'Theme',
         thumbIcon: 'Palette',
         thumbColor: '#ED6A5A',
-        icon: 'ChevronRight',
         action: () => navigation.navigate('Appearance')
       },
       {
         title: 'Account',
-        icon: 'ChevronRight',
         thumbIcon: 'Account',
         thumbColor: '#4DB3FF',
         disabled: !userStore.user,
@@ -39,7 +39,6 @@ const Settings = observer(() => {
       },
       {
         title: !userStore.user ? 'Log in / Sign up' : 'Log me out',
-        icon: 'ChevronRight',
         thumbIcon: !userStore.user ? 'Logout' : 'Logout', // TODO
         thumbColor: '#35CE8D',
         action: !userStore.user
@@ -50,14 +49,12 @@ const Settings = observer(() => {
     [
       {
         title: 'About',
-        icon: 'ChevronRight',
         thumbIcon: 'Infos',
         thumbColor: '#35CE8D',
         action: () => navigation.navigate('About')
       },
       {
         title: 'Rate the App',
-        icon: 'Twitter',
         thumbIcon: 'Star',
         thumbColor: '#4DB3FF',
         action: () => {
@@ -70,7 +67,6 @@ const Settings = observer(() => {
       },
       {
         title: 'Contact me',
-        icon: 'Twitter',
         thumbIcon: 'Pencil',
         thumbColor: '#ED6A5A',
         action: () => {
@@ -102,16 +98,14 @@ const Settings = observer(() => {
       },
       {
         title: 'Leave a tip',
-        icon: 'Twitter',
         thumbIcon: 'Money',
         thumbColor: '#FFB800',
-        action: () => navigation.navigate('Tips')
+        action: () => navigation.navigate('Tips', { products })
       }
     ],
     [
       {
         title: 'Licenses',
-        icon: 'ChevronRight',
         thumbIcon: 'Book',
         thumbColor: '#35CE8D',
         action: () => navigation.navigate('Licenses')
@@ -122,8 +116,8 @@ const Settings = observer(() => {
   const getProducts = async (itemSkus?: string[]) => {
     if (itemSkus) {
       try {
-        const test = await RNIap.getProducts(itemSkus);
-        alert(JSON.stringify(test));
+        const appProducts = await RNIap.getProducts(itemSkus);
+        setProducts(appProducts);
       } catch (err) {
         console.warn(err);
       }
